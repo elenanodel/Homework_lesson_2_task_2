8a75ec72f380650e662b91f6c86fb6ee9e2a7a8d
User <User@WIN-4I8V0TPRSH5>
2017-08-21 10:22:03 +0300

    Init commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introduction
+            Console.WriteLine("Solution of equation 4/5 * x - 20 = 0");
+            Console.WriteLine("Press 'Enter'...");
+            Console.ReadLine();
+
+            Console.WriteLine("4/5 * x = 20");
+            Console.WriteLine("x = 20 : 4/5");
+            Console.WriteLine("Press 'Enter' and you'll see the answer...");
+            Console.ReadLine();
+
+            //declaring variables and solving 
+            const int a = 20;
+            decimal b = decimal.Divide(4, 5);
+            decimal x;
+
+            x = a / b;
+
+            Console.WriteLine("x = " + x);
+            Console.ReadLine();
+        }
+    }
+}
